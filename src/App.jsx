0cf2a64b06521ddb6dd1ad806d94c1bc4745b851
malxcdev <malxcondev@gmail.com>
2024-12-06 22:57:35 -0500

    add yourcart preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import Product from "./components/Product"
-import products from "./data/products"
-import waffle from "./assets/images/image-waffle-mobile.jpg"
+import Product from "./components/Product";
+import products from "./data/products";
+import YouCart from "./components/YouCart";
+import waffle from "./assets/images/image-waffle-mobile.jpg";
 
 function App() {
-
   return (
     <>
-      <div className="p-8">
+      <div className="p-8 bg-rose-100">
         <h2 className="font-bold text-5xl mb-10 text-rose-900">Desserts</h2>
-        <Product category="Waffle" name="Waffle with Berries" price="6.50" url={waffle} />
-        {
-          products.map((products) => (
-            <Product key={products.id} category={products.category} name={products.name} price={products.price} url={products.url} />
-          ))
-        }
-
+        <Product
+          category="Waffle"
+          name="Waffle with Berries"
+          price="6.50"
+          url={waffle}
+        />
+        {products.map((products) => (
+          <Product
+            key={products.id}
+            category={products.category}
+            name={products.name}
+            price={products.price}
+            url={products.url}
+          />
+        ))}
+        <h2 className="font-bold text-4xl text-red mb-10">
+          You Cart <span>(7)</span>
+        </h2>
+        <YouCart></YouCart>
+        <YouCart></YouCart>
+        <YouCart></YouCart>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/YouCart.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import RemoveIcon from "../assets/images/icon-remove-item.svg";
+
+function YouCart() {
+  return (
+    <section>
+     
+      <div>
+        <div className="flex items-center justify-between bg-white pl-5 pr-7 py-5 rounded-lg border-b border-rose-100">
+          <div className="flex flex-col w-1/2">
+            <h5 className="font-semibold text-xl">Classic Tiramisu</h5>
+            <div className="flex justify-start gap-4">
+              <p className="text-red font-semibold text-xl">1x</p>
+              <p className="font-light text-xl text-rose-400">@$5.50</p>
+              <p className="font-medium text-rose-500 text-xl ">#5.50</p>
+            </div>
+          </div>
+          <a className="w-1/2 flex justify-end cursor-pointer">
+            <img className="border-2 border-rose-300 rounded-full p-1 h-6" src={RemoveIcon} alt="" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default YouCart;
